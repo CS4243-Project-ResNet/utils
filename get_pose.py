1b2aa04e5e8f21f96967e291167d5905ea523084
Zhu Tianqi <tianqi_zhu2002@qq.com>
2022-10-24 14:23:16 +0800

    add pose

--- get_pose.py
+++ get_pose.py
@@ -1,2 +1,2 @@
-#return joint positions(or any other abstraction) for the pose
-def get_joints(img):
+#return joint positions(or any other abstraction that describes pose properly) for the pose
+def get_pose(img):
