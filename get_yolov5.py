5463121ace9f9058173ccd64239a209df89f5310
lizchow <elizabethchow1234@gmail.com>
2022-11-09 19:45:24 +0800

    Add yolov5 detection

---
+++ get_yolov5.py
@@ -0,0 +1,33 @@
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_11_09.pt')
+model.conf = 0.1
+
+def get_classification(file_path):
+    '''
+    Classification
+    -1: None
+    0: gun
+    1: knife
+    '''
+    results = model(file_path, size=540)
+
+    if len(results.xyxy[0]) == 0:
+        return -1
+    else:
+        return int(results.xyxy[0][0][5])
+
+def get_classifcation_bounding_box(file_path):
+    results = model(file_path, size=540)
+
+    objects = []
+    for obj in results.xyxy[0]:
+        objects.append(
+            {
+                "class": int(obj[5]), 
+                "xmin": float(obj[0]),
+                "ymin": float(obj[1]),
+                "xmax": float(obj[2]),
+                "ymax": float(obj[3]),
+            })
+    return objects
